8b3ebcacfaf8222a07cbc9984f51afef4510a2cf
Andrey Pashentsev <avp200681@gmail.com>
2017-11-24 20:25:21 +0200

    fire rate

--- BattleTanks/Source/BattleTanks/Tank.h
+++ BattleTanks/Source/BattleTanks/Tank.h
@@ -40,15 +40,16 @@ protected:
 
 	UTankAimingComponent* TankAimingComponent = nullptr;
 
-	UPROPERTY(EditAnywhere, Category = Firing)
+	UPROPERTY(EditDefaultsOnly, Category = Firing)
 	float LaunchSpeed = 4000;
 
-	UPROPERTY(EditAnywhere, Category = Setup)
+	UPROPERTY(EditDefaultsOnly, Category = Setup)
 	TSubclassOf<AProjectile> ProjectileBlueprint;
 
 	// Local barrel ref for spawning projectile
 	UTankBarrel* Barrel = nullptr;
 
+	UPROPERTY(EditDefaultsOnly, Category = Firing)
 	float ReloadTimeInSeconds = 3;
 
 	double LastFireTime = 0;
