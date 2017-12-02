f10a7a2c9cb55132c0381fa21a213b6305c37696
Andrey Pashentsev <avp200681@gmail.com>
2017-12-02 12:21:03 +0200

    crosshair

--- BattleTanks/Source/BattleTanks/TankAimingComponent.h
+++ BattleTanks/Source/BattleTanks/TankAimingComponent.h
@@ -6,6 +6,15 @@
 #include "Components/ActorComponent.h"
 #include "TankAimingComponent.generated.h"
 
+// Enum for aiming state
+UENUM()
+enum class EFiringState : uint8
+{
+	Reloading,
+	Aiming,
+	Locked
+};
+
 // Forward declaration
 class UTankBarrel;
 class UTankTurret;
@@ -23,6 +32,9 @@ public:
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
+
+	UPROPERTY(BlueprintReadOnly, Category = "State")
+	EFiringState FiringState = EFiringState::Aiming;
 
 public:	
 	// Called every frame
